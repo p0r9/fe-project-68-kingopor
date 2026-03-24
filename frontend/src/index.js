8bfeef5692006105fe061c65b6b3a10890e32916
p0r9 <palathip.opor@gmail.com>
2026-03-24 12:06:46 +0700

    edit tool

---
+++ frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.css";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+  render() {
+    if (this.state.hasError) {
+      return <div style={{color: 'red', border: '5px solid red', padding: '20px'}}><h1>REACT CRASH: {this.state.error.toString()}</h1><pre>{this.state.error.stack}</pre></div>;
+    }
+    return this.props.children;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
